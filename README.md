e4def3fb7435044d41b0d2d341354d4805947ab1
Grant Timmerman <granttimmerman@gmail.com>
2014-05-10 15:24:09 -0700

    Add basic express app

---
+++ README.md
@@ -0,0 +1,8 @@
+# dubhacks.io
+
+The homepage for the University of Washington's largest hackathon.
+
+## Built with
+- Node/Express
+- Jade
+- Stylus
---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
